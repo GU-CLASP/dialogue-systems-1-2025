728906a0392e478e39ec2d373df08ac2980aabd6
unknown <cm222zr@student.lnu.se>
2025-02-24 20:41:16 +0100

    Extend Context

--- Code/src/types.ts
+++ Code/src/types.ts
@@ -4,6 +4,9 @@ import { AnyActorRef } from "xstate";
 export interface DMContext {
   spstRef: AnyActorRef;
   lastResult: Hypothesis[] | null;
+  personName: string | null;
+  meetingDate: string | null;
+  meetingTime: string | null;
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
