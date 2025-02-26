b2a0e5b6f5e7009727c587b8dd1eecb505c63b7c
m-drop682 <gusdropju@student.gu.se>
2025-02-26 20:42:05 +0100

    Added dm.ts and types.ts files

--- Code/src/types.ts
+++ Code/src/types.ts
@@ -2,8 +2,23 @@ import { Hypothesis, SpeechStateExternalEvent } from "speechstate";
 import { AnyActorRef } from "xstate";
 
 export interface DMContext {
+	
+  Answer: Hypothesis[] | null;
+  Location: Hypothesis[] | null;
+  //location: string | null | undefined;
+  Duration: Hypothesis[] | null;
+  //duration: string | null | undefined;
+  Week: Hypothesis[] | null;
+  //week: string | null | undefined;
+	//time: string | null | undefined;
+	//day: string | null | undefined;
+  //person: string | null | undefined;
+  //answer: string | null | undefined;
   spstRef: AnyActorRef;
-  lastResult: Hypothesis[] | null;
+  Person: Hypothesis[] | null;
+  Day: Hypothesis[] | null;
+  Time: Hypothesis[] | null;
+  Return_state: Hypothesis[] | null;
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
