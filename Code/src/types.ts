9e1ed1c99d0756087fae6991f769f1656a6200ca
TalhaBedirGU <talha.bedir@gu.se>
2025-02-26 23:33:09 +0100

    I changed types and dm file.

--- Code/src/types.ts
+++ Code/src/types.ts
@@ -4,6 +4,10 @@ import { AnyActorRef } from "xstate";
 export interface DMContext {
   spstRef: AnyActorRef;
   lastResult: Hypothesis[] | null;
+  person: string | null;
+  day: string | null;
+  time: string | null;
+  isFullDay: boolean | null;
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
