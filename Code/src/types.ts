fcf35e6a64e606a06e2db885fef828a4a3b41859
Ana_Paula <carvalho.anapv@gmail.com>
2025-02-26 22:03:06 +0100

    Chatbot - appointment machine + report in pdf.

--- Code/src/types.ts
+++ Code/src/types.ts
@@ -3,7 +3,15 @@ import { AnyActorRef } from "xstate";
 
 export interface DMContext {
   spstRef: AnyActorRef;
-  lastResult: Hypothesis[] | null;
+  lastResult: Hypothesis[] | null; // 
+  person: any; //
+  time: any; //
+  date: any; //
+  userInput: any; //
+  // userUtterance: string; // Adiciona a nova propriedade corretamente tipada 
+  // //Eu posso criar qualquer variable aqui e eu especifico se esse elemento existo no DM Context. Eu defino as chaves deste elemento e eu posso definor os tipos de casa um destes elementos.
+  // criar uma variavel para cada pessoa, data, hora
+
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dialogue-systems-1-2025
+
+1- Add a pdf reader to read the report-lab2.pdf file
