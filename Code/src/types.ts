19c02acb722020787aafba0645138d65d3a27685
Ana_Paula <carvalho.anapv@gmail.com>
2025-03-08 11:27:39 +0100

    Uncomment userInput and nextUtterance

--- Code/src/types.ts
+++ Code/src/types.ts
@@ -3,15 +3,14 @@ import { AnyActorRef } from "xstate";
 
 export interface DMContext {
   spstRef: AnyActorRef;
-  lastResult: Hypothesis[] | null; // 
-  person: any; //
-  time: any; //
-  date: any; //
-  userInput: any; //
-  // userUtterance: string; // Adiciona a nova propriedade corretamente tipada 
-  // //Eu posso criar qualquer variable aqui e eu especifico se esse elemento existo no DM Context. Eu defino as chaves deste elemento e eu posso definor os tipos de casa um destes elementos.
-  // criar uma variavel para cada pessoa, data, hora
-
+  lastResult: Hypothesis[] | null;
+  person: any;
+  time: any;
+  date: any;
+  userInput: any;
+  nextUtterance: string; // Keeping both properties
 }
 
-export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
+export type DMEvents = SpeechStateExternalEvent | 
+  { type: "CLICK" } | 
+  { type: "DONE" }; // Keeping both event types
