7735422788edeb751b0e5b35122c91015919e20b
5Seo0 <seoyoung0405@gmail.com>
2025-03-12 20:16:21 +0100

    Lab 4 Submission

---
+++ Code/src/dm4.ts
@@ -0,0 +1,176 @@
+import { assign, createActor, setup } from "xstate";
+import { Settings, speechstate } from "speechstate";
+import { createBrowserInspector } from "@statelyai/inspect";
+import {HARD_ASR_ENDPOINT, KEY, NLU_KEY} from "./azure";
+import { DMContext, DMEvents } from "./types";
+
+const inspector = createBrowserInspector();
+
+const azureCredentials = {
+  endpoint:
+    "https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken",
+  key: KEY,
+};
+
+const azureLanguageCredentials = {
+    endpoint: "https://asr-tts-saya.cognitiveservices.azure.com/language/:analyze-conversations?api-version=2024-11-15-preview",
+    key: NLU_KEY,
+    projectName: "lab4",
+    deploymentName: "lab4-deploy"
+};
+
+const settings: Settings = {
+  azureCredentials: azureCredentials,
+  azureLanguageCredentials : azureLanguageCredentials,
+  speechRecognitionEndpointId : HARD_ASR_ENDPOINT,
+  azureRegion: "northeurope",
+  asrDefaultCompleteTimeout: 0,
+  asrDefaultNoInputTimeout: 5000,
+  locale: "en-US",
+  ttsDefaultVoice: "en-US-DavisNeural",
+};
+
+
+interface GrammarEntry {
+  person?: string;
+  plant?: string;
+  location?: string;
+  animal?: string;
+}
+
+const grammar: { [index: string]: GrammarEntry } = {
+  vlad: { person: "Vladislav Maraev" },
+  aya: { person: "Nayat Astaiza Soriano" },
+  victoria: { person: "Victoria Daniilidou" }
+};
+
+
+function isInGrammar(utterance: string) {
+  return utterance.toLowerCase() in grammar;
+}
+
+function getPerson(utterance: string) {
+  return (grammar[utterance.toLowerCase()] || {}).person;
+}
+
+const dmMachine = setup({
+  types: {
+    /** you might need to extend these */
+    context: {} as DMContext,
+    events: {} as DMEvents,
+  },
+  actions: {
+    /** define your actions here */
+    "spst.speak": ({ context }, params: { utterance: string }) =>
+      context.spstRef.send({
+        type: "SPEAK",
+        value: {
+          utterance: params.utterance,
+        },
+      }),
+    "spst.listen": ({ context }) =>
+      context.spstRef.send({
+        type: "LISTEN",
+        value: {nlu: true}
+      }),
+  },
+}).createMachine({
+  context: ({ spawn }) => ({
+    spstRef: spawn(speechstate, { input: settings }),
+    lastResult: null,
+    lastResultInterpretation : null,
+  }),
+  id: "DM",
+  initial: "Prepare",
+  states: {
+    Prepare: {
+      entry: ({ context }) => context.spstRef.send({ type: "PREPARE" }),
+      on: { ASRTTS_READY: "WaitToStart" },
+    },
+    WaitToStart: {
+      on: { CLICK: "Greeting" },
+    },
+    Greeting: {
+      initial: "Prompt",
+      on: {
+        LISTEN_COMPLETE: [
+          {
+            target: "CheckGrammar",
+            guard: ({ context }) => !!context.lastResult,
+          },
+          { target: ".NoInput" },
+        ],
+      },
+      states: {
+        Prompt: {
+          entry: { type: "spst.speak", params: { utterance: `Ask me` } },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        NoInput: {
+          entry: {
+            type: "spst.speak",
+            params: { utterance: `I can't hear you!` },
+          },
+          on: { SPEAK_COMPLETE: "Ask" },
+        },
+        Ask: {
+          entry: { type: "spst.listen" },
+          on: {
+            RECOGNISED: {
+              actions: assign(({ event }) => {
+                return {
+                    lastResult: event.value,
+                    lastResultInterpretation : event.nluValue.topIntent,
+                };
+              }),
+            },
+            ASR_NOINPUT: {
+              actions: assign({ lastResult: null, lastResultInterpretation : null }),
+            },
+          },
+        },
+      },
+    },
+    CheckGrammar: {
+      entry: {
+        type: "spst.speak",
+        params: ({ context }) => ({
+          utterance: `You just said: ${context.lastResult![0].utterance}. And it ${
+            isInGrammar(context.lastResult![0].utterance) ? "is" : "is not"
+          } in the grammar. And the top intent is ${context.lastResultInterpretation}`,
+        }),
+      },
+      on: { SPEAK_COMPLETE: "Done" },
+    },
+    Done: {
+      on: {
+        CLICK: "Greeting",
+      },
+    },
+  },
+});
+
+const dmActor = createActor(dmMachine, {
+  inspect: inspector.inspect,
+}).start();
+
+dmActor.subscribe((state) => {
+  console.group("State update");
+  console.log("State value:", state.value);
+  console.log("State context:", state.context);
+  console.groupEnd();
+});
+
+export function setupButton(element: HTMLButtonElement) {
+  element.addEventListener("click", () => {
+    dmActor.send({ type: "CLICK" });
+  });
+  dmActor.subscribe((snapshot) => {
+    const meta: { view?: string } = Object.values(
+      snapshot.context.spstRef.getSnapshot().getMeta(),
+    )[0] || {
+      view: undefined,
+    };
+    element.innerHTML = `${meta.view}`;
+  });
+}
