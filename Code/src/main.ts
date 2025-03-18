2b3e197cd1635fdc04fe7287a30ae490ab622902
guscaroot <caroline.ott.mail@gmail.com>
2025-03-18 16:17:04 +0100

    improved visualization
    changed index.html, styles.css and  main.ts

--- Code/src/main.ts
+++ Code/src/main.ts
@@ -4,11 +4,15 @@ import viteLogo from "/vite.svg";
 import { setupButton } from "./dm_project.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
+<h1 class="heading">Voiced Crossword Puzzle</h1>  
+
+<div>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="start" type="button"></button>
     </div>
-  </div>
+    <div id="puzzle"></div>
+</div>
 `;
 
-setupButton(document.querySelector<HTMLButtonElement>("#counter")!);
+setupButton(document.querySelector<HTMLButtonElement>("#start")!);
+
