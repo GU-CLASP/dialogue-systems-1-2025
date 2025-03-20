b978da89fe6dd7af8945405d512da89b5dc34b05
Lucia Casiglio <lucia.casiglio@gmail.com>
2025-03-20 21:27:46 +0100

    lab5

--- Code/src/types.ts
+++ Code/src/types.ts
@@ -4,10 +4,14 @@ import { AnyActorRef } from "xstate";
 export interface DMContext {
   spstRef: AnyActorRef;
   lastResult: nluResponse | null;
+  plainResult: Hypothesis[] | null;
+  confidenceResult: Hypothesis[] | null;
+  confidence: boolean;
   meetingWith: string | null;
   meetingDay: string | null;
-  meetingTime: string | null;
+  meetingTime: string | null; 
   famousPerson: string | null;
+  reprompt: number;
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
